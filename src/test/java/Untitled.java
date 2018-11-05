c27403259581905b9d702894c4e8e874004afac5
Nahuel Nucera <nahuelnucera990@gmail.com>
2018-11-05 10:43:55 -0300

    test

--- src/test/java/Untitled.java
+++ src/test/java/Untitled.java
@@ -1,4 +1,6 @@
 import com.configuration.UiBaseTest;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.By;
@@ -8,17 +10,13 @@ import org.testng.annotations.*;
 public class Untitled extends UiBaseTest{
     @Test
     public void testUntitled() throws Exception {
-        driver.findElement(By.xpath("//*[@contentDescription='ReferenceApp']")).click();
-        driver.findElement(By.xpath("//*[@text='version 1']")).click();
-        driver.findElement(By.xpath("//*[@contentDescription='ReferenceApp']")).click();
-        driver.findElement(By.xpath("//*[@text='Native Components']")).click();
-        driver.findElement(By.xpath("//*[@text='Content Scrolling']")).click();
-        driver.swipe(559, 781, 728, 71, 522);
-        driver.swipe(506, 881, 621, 359, 500);
-        driver.findElement(By.xpath("//*[@contentDescription='ReferenceApp']")).click();
-        driver.findElement(By.xpath("//*[@text='Input Controls']")).sendKeys("Hola mundo");
-        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Checkbox' and @class='android.widget.TextView']")));
-        driver.findElement(By.xpath("//*[@text='Checkbox' and @class='android.widget.TextView']")).click();
-        driver.findElement(By.xpath("//*[@id='input_checkbox']")).click();
+        MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("ReferenceApp");
+        el1.click();
+        Thread.sleep(5000);
+        MobileElement el2 = (MobileElement) driver.findElementByXPath("(//android.widget.TextView[@content-desc=\"Row Category Name\"])[4]");
+        el2.click();
+        Thread.sleep(5000);
+        MobileElement el3 = (MobileElement) driver.findElementByAccessibilityId("Text Input Control");
+        el3.sendKeys("Hola mundo");
     }
 }
